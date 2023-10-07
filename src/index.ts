2f7697fbdf1b0bb1afde49f3e892c2b955e5ea37
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-10-07 11:42:46 +0800

    Update dependency

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,38 @@ function sayHelloWorld(req: Request, res: Response) {
     res.send('Hello World!')
 }
 
+function add(req: Request, res: Response) {
+    try {
+        let result = 0
+        const { first, second } = req.query
+        console.log('First: ', Number(first))
+        console.log('Second: ', Number(second))
+        result = Number(first) + Number(second)
+
+        if (Number(first) < 0) {
+            console.log('NEGATIVE SYA')
+            throw new Error('Negative numbers not allowed')
+        }
+
+        if (isNaN(result)) {
+            throw new Error('Invalid format, we only accept actual numbers')
+        }
+
+        res.status(200)
+        res.json(result)
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400)
+            res.json(error.message)
+        }
+    }
+}
+
 app.get('/', sayHelloWorld)
+
+app.get('/add', sayHelloWorld)
+app.post('/add', add)
+
 app.get('/test', (req, res) => {
     res.json({
         status: 200,
